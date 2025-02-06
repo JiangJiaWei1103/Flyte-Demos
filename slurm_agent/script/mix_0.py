28af67d9de9b5005b2187e95ffb63bb04733b72e
JiangJiaWei1103 <waynechuang97@gmail.com>
2025-02-06 22:15:52 +0800

    Add a mix example with SlurmTask and SlurmShellTask
    Two tasks are executed in a row
    
    Signed-off-by: JiangJiaWei1103 <waynechuang97@gmail.com>

---
+++ slurm_agent/script/mix_0.py
@@ -0,0 +1,54 @@
+import os
+
+from flytekit import workflow
+from flytekitplugins.slurm import Slurm, SlurmTask, SlurmRemoteScript, SlurmShellTask
+
+
+echo_job = SlurmTask(
+    name="test-slurm",
+    task_config=SlurmRemoteScript(
+        slurm_host="aws",
+        batch_script_path="/home/ubuntu/test_slurm_job.sh",
+        sbatch_conf={
+            "partition": "debug",
+            "job-name": "tiny-slurm",
+        }
+    )
+)
+
+shell_task = SlurmShellTask(
+    name="test-shell",
+    script="""#!/bin/bash
+# We can define sbatch options here, but using sbatch_conf can be more neat
+echo "Run a Flyte SlurmShellTask...\n"
+# Run a python script on Slurm
+# Activate the virtual env first if any
+# python3 <path_to_python_script>
+""",
+    task_config=Slurm(
+        slurm_host="aws",
+        sbatch_conf={
+            "partition": "debug",
+            "job-name": "tiny-slurm",
+        }
+    ),
+)
+
+
+@workflow
+def wf():
+    echo_job()
+    shell_task()
+
+
+if __name__ == "__main__":
+    from flytekit.clis.sdk_in_container import pyflyte
+    from click.testing import CliRunner
+
+    runner = CliRunner()
+    path = os.path.realpath(__file__)
+
+    # Local run
+    print(f">>> LOCAL EXEC <<<")
+    result = runner.invoke(pyflyte.main, ["run", path, "wf"])
+    print(result.output)
