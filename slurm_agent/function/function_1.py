818fe58568c69885ccda7c6261b0fd4b29573482
JiangJiaWei1103 <waynechuang97@gmail.com>
2025-02-06 21:45:30 +0800

    Add an example for Slurm fn task with script
    Signed-off-by: JiangJiaWei1103 <waynechuang97@gmail.com>

---
+++ slurm_agent/function/function_1.py
@@ -0,0 +1,62 @@
+import os
+
+from flytekit import task, workflow
+from flytekitplugins.slurm import SlurmFunction 
+
+
+@task(
+    task_config=SlurmFunction(
+        slurm_host="aws",
+        srun_conf={
+            "partition": "debug",
+            "job-name": "fn-task",
+            "output": "/home/ubuntu/fn_task.log"
+        },
+        script="""
+#!/bin/bash
+
+# == Pre-Execution ==
+echo "Hello, world!"
+
+# Setup env vars
+export MY_ENV_VAR=123
+
+# Activate virtual env
+. /home/ubuntu/.cache/pypoetry/virtualenvs/demo-poetry-RLi6T71_-py3.12/bin/activate
+
+# == Execute Flyte Task Function ==
+{task.fn}
+
+# == Post-Execution ==
+echo "Success!!"
+"""
+    )
+)
+def plus_one(x: int) -> int: 
+    print(os.getenv("MY_ENV_VAR"))
+    return x + 1
+
+
+@task
+def greet(year: int) -> str:
+    return f"Hello {year}!!!"
+
+
+@workflow
+def wf(x: int) -> str:
+    x = plus_one(x=x)
+    msg = greet(year=x)
+    return msg
+
+
+if __name__ == "__main__":
+    from flytekit.clis.sdk_in_container import pyflyte
+    from click.testing import CliRunner
+
+    runner = CliRunner()
+    path = os.path.realpath(__file__)
+
+    # Local run
+    print(f">>> LOCAL EXEC <<<")
+    result = runner.invoke(pyflyte.main, ["run", "--raw-output-data-prefix", "s3://my-flyte-slurm-agent", path, "wf", "--x", 2024])
+    print(result.output)
