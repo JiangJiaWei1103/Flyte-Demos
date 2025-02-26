f6cedecc0f436ca512127511f62ce99f9405e4e2
JiangJiaWei1103 <waynechuang97@gmail.com>
2025-02-26 23:54:37 +0800

    Add training logic and pass train function task
    1. Complete training logic
    2. Training logic checked
    3. `train` function task checked
        * Well-trained model weights is successfully returned
    
    Signed-off-by: JiangJiaWei1103 <waynechuang97@gmail.com>

--- slurm_agent/model_training/main.py
+++ slurm_agent/model_training/main.py
@@ -1,12 +1,16 @@
 """
 A beautiful MNIST model training example using the Slurm agent.
+
+* Uploading and downloading processed datasets take too much time.
 """
 import os
-from typing import Tuple
+from pathlib import Path
+from typing import Dict, Optional
 
+import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from flytekit import task, workflow
 from flytekit.types.file import FlyteFile
 from flytekitplugins.slurm import SlurmFunction
@@ -36,10 +40,13 @@ echo "Process and build torch datasets..."
 """
     )
 )
-def process_data(raw_data_path: str) -> Tuple[Dataset, Dataset]:
-    tr_ds, val_ds = get_dataset(download_path=raw_data_path)
+def process_data(raw_data_path: str) -> str:
+    # Bypass data preprocessing here
+    # tr_ds, val_ds = get_dataset(download_path=raw_data_path)
+    # return tr_ds, val_ds
+    proc_data_path = raw_data_path
 
-    return tr_ds, val_ds
+    return proc_data_path
 
 
 @task(
@@ -63,13 +70,19 @@ echo "Run main training and evaluation processes..."
     )
 )
 def train(
-    tr_ds: Dataset,
-    val_ds: Dataset,
+    data_path: str,
     epochs: int = 5,
     batch_size: int = 32,
     lr: float = 1e-3,
+    ckpt_path: Optional[str] = None,
+    debug: bool = False,
 ) -> FlyteFile:
+    ckpt_path = Path("./output") if ckpt_path is None else Path(ckpt_path)
+    ckpt_path.mkdir(exist_ok=True)
+    model_path = ckpt_path / "model.pth"
+
     # Build dataloaders
+    tr_ds, val_ds = get_dataset(download_path=data_path)
     tr_loader = DataLoader(tr_ds, batch_size=batch_size, shuffle=True, drop_last=True)
     val_loader = DataLoader(val_ds, batch_size=batch_size*4, shuffle=False)
 
@@ -86,33 +99,50 @@ def train(
     lr_skd = None
 
     # Run training and evaluation
+    best_score = 1e16
     for ep in range(epochs):
         tr_loss = train_epoch(
             tr_loader=tr_loader, 
             model=model, 
             loss_fn=loss_fn, 
             optimizer=optimizer,
+            debug=debug,
         )
-        val_loss, acc = eval_epoch()    
+        val_loss, acc = eval_epoch(eval_loader=val_loader, model=model, loss_fn=loss_fn, debug=debug)
 
-        # Record the model ckpt
+        # Save model ckpt
+        if val_loss < best_score:
+            best_score = val_loss
+            torch.save(model.state_dict(), model_path)
 
-    # return weights
+        print(f"Epoch [{ep+1}/{epochs}] TRAIN LOSS {tr_loss:.4f} | VAL LOSS {val_loss:.4f} | ACC {acc:.4f}")
+
+    return FlyteFile(path=model_path)
 
 
-# @task
-# def run_infer() -> Dict[str, float]:
-#     return prf_report
+@task
+def run_infer(model_path: FlyteFile) -> Dict[str, float]:
+    # Load model
+    model = Model()
+    model.load_state_dict(torch.load(model_path.download()))
+
+    # Run inference
+    prf_report = {}
+
+    return prf_report
 
 
-# @workflow
-# def dl_wf(
-#     raw_data_path: str,
-# ) -> Dict[str, float]:
-#     tr_ds, val_ds = process_data(raw_data_path=raw_data_path)
-#     output_path = train()
-#     prf_report = run_infer()
-#     return prf_report
+@workflow
+def dl_wf(
+    raw_data_path: str,
+    epochs: int = 1,
+    debug: bool = False
+) -> Dict[str, float]:
+    proc_data_path = process_data(raw_data_path=raw_data_path)
+    output_path = train(data_path=proc_data_path, epochs=epochs, debug=debug)
+    prf_report = run_infer(model_path=output_path)
+
+    return prf_report
 
 
 if __name__ == "__main__":
@@ -124,6 +154,5 @@ if __name__ == "__main__":
 
     # Local run
     print(f">>> LOCAL EXEC <<<")
-    result = runner.invoke(pyflyte.main, ["run", "--raw-output-data-prefix", "s3://my-flyte-slurm-agent", path, "process_data", "--raw_data_path", "/tmp/torch_data"])
+    result = runner.invoke(pyflyte.main, ["run", "--raw-output-data-prefix", "s3://my-flyte-slurm-agent", path, "dl_wf", "--raw_data_path", "/tmp/torch_data"])
     print(result.output)
-
--- slurm_agent/model_training/trainer.py
+++ slurm_agent/model_training/trainer.py
@@ -1,11 +1,143 @@
 """
 Define main training logic.
+
+* [ ] Handle `device`
 """
+import gc
+from typing import Tuple
+
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
 
 
-def train_epoch():
-    pass
+def train_epoch(
+    tr_loader: DataLoader,
+    model: nn.Module,
+    loss_fn: nn.Module,
+    optimizer: Optimizer,
+    debug: bool = False
+) -> float:
+    """Run training for one epoch.
+
+    Args:
+        tr_loader: Training dataloader.
+        model: Model instance.
+        loss_fn: Loss criterion.
+        optimizer: Optimizer.
+        debug: If True, run one batch only.
+
+    Returns:
+        The average training loss over batches.
+    """
+    tr_loss_tot = 0.0
+
+    model.train()
+    for i, batch_data in tqdm(enumerate(tr_loader), total=len(tr_loader)):
+        optimizer.zero_grad(set_to_none=True)
+
+        # Retrieve batched raw data
+        x, y = batch_data
+        inputs = {"x": x}
+
+        # Forward pass
+        logits = model(inputs)
+
+        # Derive loss
+        loss = loss_fn(logits, y)
+        tr_loss_tot += loss.item()
+
+        # Backpropagation
+        loss.backward()
+
+        optimizer.step()
+
+        del x, y, inputs, logits 
+        _ = gc.collect()
+
+        if debug:
+            break
+
+    tr_loss_avg = tr_loss_tot / len(tr_loader)
+
+    return tr_loss_avg    
 
 
-def eval_epoch():
-    pass
+@torch.no_grad()
+def eval_epoch(
+    eval_loader: DataLoader, 
+    model: nn.Module,
+    loss_fn: nn.Module,
+    debug: bool = False
+) -> Tuple[float, float]:
+    """Run evaluation for one epoch.
+
+    Args:
+        eval_loader: Evaluation dataloader.
+        model: Model instance.
+        loss_fn: Loss criterion.
+        debug: If True, run one batch only.
+
+    Returns:
+        A tuple (eval_loss_avg, acc), where eval_loss_avg is the average evaluation loss over batches
+        and acc is the accuracy.
+    """
+    eval_loss_tot = 0
+    y_true, y_pred = [], []
+
+    model.eval()
+    for i, batch_data in tqdm(enumerate(eval_loader), total=len(eval_loader)):
+        # Retrieve batched raw data
+        x, y = batch_data
+        inputs = {"x": x}
+
+        # Forward pass
+        logits = model(inputs)
+
+        # Derive loss
+        loss = loss_fn(logits, y)
+        eval_loss_tot += loss.item()
+
+        # Record batched output
+        y_true.append(y.detach())
+        y_pred.append(logits.detach())
+
+        del x, y, inputs, logits 
+        _ = gc.collect()
+
+        if debug:
+            break
+
+    eval_loss_avg = eval_loss_tot / len(eval_loader)
+
+    # Derive accuracy
+    y_true = torch.cat(y_true, dim=0)
+    y_pred = torch.cat(y_pred, dim=0)
+    y_pred = torch.argmax(y_pred, dim=1)
+    acc = (y_true == y_pred).sum() / len(y_true)
+
+    return eval_loss_avg, acc
+
+
+if __name__ == "__main__":
+    from dataset import get_dataset
+    from model import Model
+
+    B, C, H, W = 32, 1, 28, 28
+    tr_ds, val_ds = get_dataset("./tmp_data")
+    tr_loader = DataLoader(tr_ds, batch_size=B, shuffle=True, drop_last=True)
+    val_loader = DataLoader(val_ds, batch_size=B*4, shuffle=False, drop_last=False)
+    for i, batch_data in enumerate(tr_loader):
+        x, y = batch_data
+        assert x.shape == torch.Size([B, C, H, W])
+        assert y.shape == torch.Size([B, ])
+
+    model = Model()
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    tr_loss = train_epoch(tr_loader, model, loss_fn, optimizer, debug=True)
+    val_loss, acc = eval_epoch(val_loader, model, loss_fn, debug=True)
+    print(f"TRAIN LOSS {tr_loss:.4f} | VAL LOSS {val_loss:.4f} | ACC {acc:.4f}")
